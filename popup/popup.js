3d33cf024c6934bba29e88835f600b87b99875df
Manish <manisharadwad@gmail.com>
2020-03-10 13:00:19 +0530

    Updated permissions

--- popup/popup.js
+++ popup/popup.js
@@ -5,7 +5,20 @@ browser.tabs.onActivated.addListener(handleActivated);
 
 function handleUpdated(tabId, changeInfo, tabInfo) {
     if (changeInfo.url) {
-      // update extensions' list on current tab
+      // update extensions' list on current tab if url changes
     }
   }
-  browser.tabs.onUpdated.addListener(handleUpdated);
+browser.tabs.onUpdated.addListener(handleUpdated);
+
+// if user clicks on "Details" button in popup, it will open the Extension Activity Page
+// if not nothing happens
+document.addEventListener("click", function(e) {
+  if (!e.target.classList.contains("details")) {
+    return;
+  }
+
+  browser.tabs.create({
+    url: "extension_activity_page/activity_page.html"
+  });
+
+});
